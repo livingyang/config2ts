42ad2ac5a9ced7a4f276143839ef2cdcf5ba8a94
livinglhmli <lhm.cold@163.com>
2016-11-09 17:35:19 +0800

    更新配置文件

--- config/data.ts
+++ config/data.ts
@@ -4,7 +4,23 @@ interface data {
     bool: number;
     num: number;
 };
-export let dataRows: data[] =
-[ { id: 1, name: 'xxx', bool: 1, num: 111 },
-  { id: 2, name: 'xxx', bool: 0, num: 222 },
-  { id: 3, name: '', bool: 0, num: 0 } ]
+export let dataRows: data[] = [
+    {
+        "id": 1,
+        "name": "xxx",
+        "bool": 1,
+        "num": 111
+    },
+    {
+        "id": 2,
+        "name": "xxx",
+        "bool": 0,
+        "num": 222
+    },
+    {
+        "id": 3,
+        "name": "",
+        "bool": 0,
+        "num": 0
+    }
+];
--- dist/config/data.js
+++ dist/config/data.js
@@ -1,6 +1,23 @@
 "use strict";
 ;
-exports.dataRows = [{ id: 1, name: 'xxx', bool: 1, num: 111 },
-    { id: 2, name: 'xxx', bool: 0, num: 222 },
-    { id: 3, name: '', bool: 0, num: 0 }];
+exports.dataRows = [
+    {
+        "id": 1,
+        "name": "xxx",
+        "bool": 1,
+        "num": 111
+    },
+    {
+        "id": 2,
+        "name": "xxx",
+        "bool": 0,
+        "num": 222
+    },
+    {
+        "id": 3,
+        "name": "",
+        "bool": 0,
+        "num": 0
+    }
+];
 //# sourceMappingURL=data.js.map
