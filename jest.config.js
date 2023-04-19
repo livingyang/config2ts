d305a6c7c37bcbd22bac88c8f928ab561308887b
huanming li <lhm.cold@163.com>
2023-04-20 03:47:14 +0800

    修复d3的编译错误

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    'd3': '<rootDir>/node_modules/d3/dist/d3.min.js',
-  }
+  testEnvironment: 'node'
 };
